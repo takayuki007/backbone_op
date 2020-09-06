d09fdcfd3d23ec5f1862dca56197f5b649ab8db9
TAKAYUKI MIYOSHI <pico.pico.hammer@icloud.com>
2020-09-06 17:00:02 +0900

    検索フォームからtodoリストを検索できるようにした。

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ let ItemModel = Backbone.Model.extend({
     defaults:{
         text: '',
         isDone: false,
-        editMode: false
+        editMode: false,
+        show: true,
     }
 });
 
@@ -19,6 +20,14 @@ let Form = Backbone.Model.extend({
 });
 
 let form = new Form();
+
+let Search = Backbone.Model.extend({
+    defaults: {
+        val: '',
+    }
+})
+
+let search = new Search();
 
 //collection
 let List = Backbone.Collection.extend({
@@ -141,3 +150,45 @@ let FormView = Backbone.View.extend({
     }
 });
 new FormView();
+
+let SearchView = Backbone.View.extend({
+    el: $('.js-search'),
+    template: _.template($('#template-search').html()),
+    model: search,
+    collection: list,
+    events: {
+        'keyup .js-get-search': 'Search'
+    },
+    initialize: function ()
+    {
+        _.bindAll(this, 'render', 'Search','searchList');
+        this.render();
+    },
+    Search: function ()
+    {
+        this.model.set({val: $('.js-get-search').val()});
+        this.searchList();
+    },
+    searchList: function ()
+    {
+        let regExp = new RegExp('^'+ this.model.get('val'));
+        this.collection.each(function(model,i){
+            let val = model.get('text');
+            if(val.match(regExp)){
+                console.log(`${i}個目の${val}がヒット`);
+                model.set({show: true});
+            }else{
+                console.log(`${i}個目の${val}がヒットしていない`);
+                model.set({show: false});
+            }
+        })
+    },
+    render: function ()
+    {
+        let template = this.template(this.model.attributes);
+        this.$el.html(template);
+        return this;
+    }
+});
+
+new SearchView();
